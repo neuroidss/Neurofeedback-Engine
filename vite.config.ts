4cdc862997547ab9e0840f647830f98fbf3a5ede
dmitry-sukhoruchkin <dmitry.sukhoruchkin@gmail.com>
2025-11-11 13:41:33 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
+/home/dmitryneuro/git.neuroidss/Singularity-Agent-Factory/vite.config.ts        
+import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import { URL, fileURLToPath } from 'url';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Neurofeedback-Engine/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
-          '@': fileURLToPath(new URL('.', import.meta.url)),
+          '@': path.resolve(__dirname, '.'),
         }
       }
     };
 });
+
